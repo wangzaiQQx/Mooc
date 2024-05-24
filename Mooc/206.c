b3c000d19cf9385014996190d0e25019d4eba5a5
wangzaiQQx <2503348218@qq.com>
2024-05-24 15:44:58 +0800

    Height projections

---
+++ Mooc/206.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    // boheight = (faheight + moheight)*0.54
+    // giheight = (faheight*0.923 + moheight)*0.5
+    printf("Height of xiao ming:%d\nHeight of xiao hong:%d\n",181,154);
+    return 0;
+}
