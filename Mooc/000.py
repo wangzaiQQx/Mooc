1c9645891aa546a149ca774bb1d91b95f2766172
wangzaiQQx <2503348218@qq.com>
2024-05-30 20:22:57 +0800

    草稿1

--- Mooc/000.py
+++ Mooc/000.py
@@ -1,1 +1,15 @@
-print(0.1*10+0.075*10+0.05*20+0.03*20+0.015*40+0.01*(789-100))
+x=3
+a = 1
+n = 0
+
+def fun(x, a = 1):
+    for n in range(1,x+1):
+        a *= n
+    return a
+
+while abs(a)>0.00001:
+    a = (-1)**n * x**(2*n+1) / fun(2*n+1)
+    b = x**(2*n+1)
+    c = fun(2*n+1)
+    n += 1
+    print(a,b,c)
