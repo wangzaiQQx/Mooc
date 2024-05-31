6412b8226f561ab1340eb74254c65a0286b29878
wangzaiQQx <2503348218@qq.com>
2024-05-31 17:37:54 +0800

    网购打折商品V2.0

---
+++ Mooc/704.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    float p,c;
+
+    printf("Input payment:");
+    scanf("%f", &p);
+
+    if (p < 100) c = p;
+    if (p < 200 && p >= 100) c = 0.95*p;
+    if (p < 500 && p >= 200) c = 0.92*p;
+    if (p < 1000 && p >= 500) c = 0.9*p;
+    if (p >= 1000) c = 0.85*p;
+
+    printf("price = %.1f\n", c);
+}
