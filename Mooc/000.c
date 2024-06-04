f4db31dc3bcdc44c5e780cad6e1b5a7c00c446dc
wangzaiQQx <2503348218@qq.com>
2024-06-04 09:18:57 +0800

    c语言草稿

--- Mooc/000.c
+++ Mooc/000.c
@@ -1,11 +1,69 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-long Fact(int n){
-    if (n < 0) return -1;
-    if (n==0||n==1) return 1;
-    return n*Fact(n-1);
+// //阶乘
+// long Fact(int n){
+//     if (n < 0) return -1;
+//     if (n==0||n==1) return 1;
+//     return n*Fact(n-1);
+// }
+
+// //斐波那契数列
+// long Fibo(int n){
+//     if (n <= 1) return n;
+//     return Fibo(n-1)+Fibo(n-2);
+// }
+
+// int main(){
+//     for (int n=0 ;n<=9 ; n++){
+//         printf("%d\n", Fibo(n));
+//     }
+// }
+
+// #include <stdio.h>
+// int Fun(int m)
+// {   
+//    static int n = 0;
+//     m /= 2;
+//     m = m * 2;
+
+//     if (m)
+//     {   
+//         n *= m;
+//         return(Fun(m - 2));
+//     }
+//     else 
+//         return n;
+// }
+
+
+// int main()
+// {   
+//     int a, i;
+    
+//     for (i = 0; i < 2; i++)
+//     {    
+//         a = Fun(4 + i);
+//         printf("%d\n", a);
+//     }
+    
+//     return 0;
+// }
+
+#include <stdio.h>
+int Square(int i){
+    return i * i;
 }
 
-int main(){
-    printf("%d", Fact(9));
+int main(void){
+    int i = 0;
+    i = Square(i);
+
+    for ( ; i<3; i++){
+        static int i = 1;
+        i += Square(i);
+        printf("%d,", i);
+    }
+    
+    printf("%d\n", i);
+    return 0;
 }
