f0c0ec6a92ce00c4cbfd5c62ef2028d5842a1e6a
wangzaiQQx <2503348218@qq.com>
2024-06-04 09:57:10 +0800

    摘苹果

---
+++ Mooc/801.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int GetApple(int a[], int height, int n) {
+    int count = 0; 
+    for (int i = 0; i < n; ++i) {
+        if (a[i] <= height + 30) count++;
+    }
+    return count;
+}
+
+int main() {
+    int a[10]; 
+    int height; 
+    int n = 10; 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    scanf("%d", &height);
+    printf("%d", GetApple(a, height, n));
+
+    return 0;
+}
