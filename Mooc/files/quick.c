29679ef80ad4af54d2b5f3b01b52ee311c6b31ea
wangzaiQQx <2503348218@qq.com>
2024-06-04 09:18:06 +0800

    .h与.c的联系

---
+++ Mooc/files/quick.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
+
+int main(){
+    printf("%f\n", Q_rsqrt(2));
+    printf("%f", 1/sqrt(2));
+    return 0;
+}
---
+++ Mooc/files/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "max.h"
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = Max(a, b);
+    
+    printf("hello world\n");
+    printf("%d\n", c);
+
+    return 0;
+}
