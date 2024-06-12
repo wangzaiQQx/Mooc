ab83c0a76b2b1582789d8f57a1a6c4182c67f8d9
wangzaiQQx <2503348218@qq.com>
2024-06-12 11:59:43 +0800

    重复数字检查

--- Mooc/901.c
+++ Mooc/901.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-int main(){
-    int num;
-    int *p;
-    printf("Input n:\n");
-    scanf("%ld", &num);
 
+int CountRepeatNum(int count[], int n);
+
+int CountRepeatNum(int count[], int n) {
+
+    while (n > 0) {
+        if (count[n%10]++ > 0) return n%10;
+        n /= 10;
+    }
+    
+    return -1;
+}
+
+int main(){
+    int count[9]={0};
+    long n;
+    printf("Input n:\n");
+    scanf("%ld", &n);
+    if (CountRepeatNum(count,n)==-1) printf("No repeated digit!\n");
+    else printf("Repeated digit!\n");
 
 }
